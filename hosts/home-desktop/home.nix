9772eec33a41fb5cfdcea16c9b2b31d1ed622468
Hyper <hypercrusher@proton.me>
2025-04-12 22:47:45 -0700

    New Firejail Wrapper for wps
    Now resides in its own directory and file.
    Now creates DesktopFiles
    Now disabels the built in desktop files

--- hosts/home-desktop/home.nix
+++ hosts/home-desktop/home.nix
@@ -1,48 +1,10 @@
-{ pkgs, inputs, lib, ... }:
-
-let
-  makeFirejailWrapper =
-    pkg: originalBin: newName: extraArgs:
-    pkgs.writeShellScriptBin newName ''
-      exec ${pkgs.firejail}/bin/firejail --net=none ${lib.escapeShellArgs extraArgs} ${pkg}/bin/${originalBin} "$@"
-    '';
-
-  firejailedEt = makeFirejailWrapper pkgs.wpsoffice "et" "spreadsheets" [ ];
-  firejailedWps = makeFirejailWrapper pkgs.wpsoffice "wps" "writer" [ ];
-  firejailedWpp = makeFirejailWrapper pkgs.wpsoffice "wpp" "powerpoint" [ ];
-  firejailedWpspdf = makeFirejailWrapper pkgs.wpsoffice "wpspdf" "pdf" [ ];
-
-in
+{ pkgs, ... }:
 {
   home = {
     username = "Hyper";
     homeDirectory = "/home/hyper";
     stateVersion = "23.11";
   };
-
-  home.packages = [
-    firejailedEt
-    firejailedWps
-    firejailedWpp
-    firejailedWpspdf
-  ];
-
-  home.file.".local/bin/spreadsheets" = {
-    source = "${firejailedEt}/bin/spreadsheets";
-    executable = true;
-  };
-  home.file.".local/bin/word" = {
-    source = "${firejailedWps}/bin/writer";
-    executable = true;
-  };
-  home.file.".local/bin/presentation" = { source = "${firejailedWpp}/bin/powerpoint";
-    executable = true;
-  };
-  home.file.".local/bin/pdf" = {
-    source = "${firejailedWpspdf}/bin/pdf";
-    executable = true;
-  };
-
 
   dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
