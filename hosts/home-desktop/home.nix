74f17d18e1ae7438f43f36b163bb2bdafb30a574
Hyper <hypercrusher@proton.me>
2025-04-12 22:49:17 -0700

    Added Monitor Setup to hyrpland
    Monitor setup is now done in home.nix so that it is separated from the
    dotfiles repo since this will change with every system

--- hosts/home-desktop/home.nix
+++ hosts/home-desktop/home.nix
@@ -19,6 +19,9 @@
       pkgs.hyprlandPlugins.hyprsplit
     ];
     extraConfig = ''
+    monitor=DP-2,2560x1440@144,auto-right,1
+    monitor=DP-3,2560x1440@144,auto-left,1
+
     source = ~/.config/hypr/hyprland-nix.conf
     '';
   };
